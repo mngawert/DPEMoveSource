88a597a38b996d317a8b607476558512e02ec77c
Montri <montrin@gmail.com>
2020-01-19 22:20:10 +0700

    fixed stadium bug.

--- DPEMoveWeb/Controllers/StadiumController.cs
+++ DPEMoveWeb/Controllers/StadiumController.cs
@@ -1,21 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using DPEMoveDAL.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DPEMoveWeb.Controllers
 {
     public class StadiumController : Controller
     {
+        private readonly AppDbContext _context;
+        private readonly ILogger<StadiumController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public StadiumController(AppDbContext context, ILogger<StadiumController> logger, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        private async Task<int> GetLoginAppUserId()
+        {
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId != null)
+            {
+                var user = await _userManager.FindByIdAsync(userId);
+                if (user != null)
+                {
+                    _logger.LogDebug("user.AppUserId={0}", user.AppUserId);
+
+                    return user.AppUserId;
+                }
+            }
+            return -1;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
             ViewBag.routeId = id;
+            ViewBag.AppUserId = await GetLoginAppUserId();
 
             return View();
         }
