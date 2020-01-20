b4bfe85d673a2f52d56fb47ad33b413d21384d8b
Montri <montrin@gmail.com>
2020-01-20 08:40:40 +0700

    add PSN create template.

--- DPEMoveWeb/Controllers/PSNController.cs
+++ DPEMoveWeb/Controllers/PSNController.cs
@@ -13,6 +13,10 @@ namespace DPEMoveWeb.Controllers
             return View();
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
         public IActionResult Edit(int id)
         {
             return View();
