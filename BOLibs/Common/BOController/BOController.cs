5ed68e04bd214cf64e52566276c932d0a15275ce
Pheeradech Bharkpises <pheeradech.bharkpises@gmail.com>
2020-02-06 08:23:07 +0700

    Edit base controller

---
+++ BOLibs/Common/BOController/BOController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BOLibs.Common.BOController
+{
+    public class BOController : Controller
+    {
+        private string SESSION_DPE_TOKEN = "SESSION_DPE_TOKEN";
+
+        public BOController() : base()
+        {
+
+        }
+
+        protected string CallDPEDataAPI(string strURL, string strFormData)
+        {
+            if (HttpContext.Session.GetString(SESSION_DPE_TOKEN) == null)
+            {
+
+            }
+
+            return "";
+        }
+
+        protected string GetDPEToken()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+            }
+
+            return "";
+        }
+    }
+}
--- DPEMoveWeb/Controllers/HomeController.cs
+++ DPEMoveWeb/Controllers/HomeController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BOLibs.Common.BOController;
 
 namespace DPEMoveWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BOController
     {
         public IActionResult Index()
         {
