93e98599dbdf3d635365ec23bd9f44798ede867b
Montri <montrin@gmail.com>
2020-02-21 14:57:11 +0700

    Add Policy and Term.

---
+++ DPEMoveWeb/Controllers/PolicyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DPEMoveDAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DPEMoveWeb.Controllers
+{
+    public class PolicyController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PolicyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var q = _context.CpeConfig.Where(a => a.Name == "Policy").FirstOrDefault();
+
+            return View(q);
+        }
+    }
+}
---
+++ DPEMoveWeb/Controllers/TemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DPEMoveDAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DPEMoveWeb.Controllers
+{
+    public class TermController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TermController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var q = _context.CpeConfig.Where(a => a.Name == "Terms and Condition").FirstOrDefault();
+
+            return View(q);
+        }
+    }
+}
