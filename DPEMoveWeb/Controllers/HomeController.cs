760f7347af7976db53a0a3472905a4cf36527bed
Montri <montrin@gmail.com>
2020-03-10 10:59:04 +0700

    remove BOLibs.

--- DPEMoveWeb/Controllers/HomeController.cs
+++ DPEMoveWeb/Controllers/HomeController.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using BOLibs.Common.BOController;
-using BOLibs.Common.BOObject;
+//using BOLibs.Common.BOController;
+//using BOLibs.Common.BOObject;
 
 namespace DPEMoveWeb.Controllers
 {
-    public class HomeController : BOController
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult GetStadiumData()
-        {
-            PostData postData = new PostData();
-            postData.Add("STADIUM_NAME", "โรงเรียนบ้านหอระฆัง");
+        //public ActionResult GetStadiumData()
+        //{
+        //    PostData postData = new PostData();
+        //    postData.Add("STADIUM_NAME", "โรงเรียนบ้านหอระฆัง");
 
 
-            return Content(base.CallDPEDataAPI("http://data.dpe.go.th/api/stadium/address/getStadium", postData));
-        }
+        //    return Content(base.CallDPEDataAPI("http://data.dpe.go.th/api/stadium/address/getStadium", postData));
+        //}
     }
 }
